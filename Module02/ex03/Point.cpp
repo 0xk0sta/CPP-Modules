d8226f0dbac576627dfb6366d9c670f1abb39958
acostal- <acostalcardenas@gmail.com>
2022-02-27 22:25:53 +0100

    FIX

--- Module02/ex03/Point.cpp
+++ Module02/ex03/Point.cpp
@@ -1,30 +1,21 @@
 #include "Point.hpp"
 
-Point::Point() : _x(0), _y(0) {
-	//std::cout << "Default constructor called" << std::endl;
-}
+Point::Point() : _x(0), _y(0) {}
 
 Point::Point(const float x, const float y) : _x(x), _y(y) {}
 
-Point::~Point() {
-	//std::cout << "Destructor called" << std::endl;
-}
+Point::~Point() {}
 
-Point::Point(const Point &src) : _x(src._x), _y(src._y) {
-//	std::cout << "Copy constructor called" << std::endl;
-}
+Point::Point(const Point &src) : _x(src._x), _y(src._y) {}
 
 Point &Point::operator=(const Point &) {
-//	std::cout << "Asignation operator called" << std::endl;
 	return *this;
 }
 
 float Point::getX() const {
-//	std::cout << "getX member function called" << std::endl;
-	return this->_x.getRawBits();
+	return this->_x.toFloat();
 }
 
 float Point::getY() const {
-//	std::cout << "getY member function called" << std::endl;
-	return this->_y.getRawBits();
+	return this->_y.toFloat();
 }
